83e48b07aecc3bd260e8425f13f87077ab9f6009
chauhansarthakiitd <93792631+BioTod@users.noreply.github.com>
2025-01-02 09:00:00 +0530

    Initial 2025 environment setup

---
+++ tapeagents/src/environment.py
@@ -0,0 +1,7 @@
+def setup_environment():
+    print("Setting up TapeAgents environment 2025.1")
+    return {
+        "version": "2025.1",
+        "cuda_available": True,
+        "debug_mode": False
+    }
